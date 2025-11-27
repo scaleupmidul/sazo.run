c15ab6cccc27309bb8b9359cf7ec3112edbf05ce
scaleupmidul <scaleup.midul@gmail.com>
2025-11-27 16:09:11 +0600

    Update AdminSettingsPage.tsx

--- pages/admin/AdminSettingsPage.tsx
+++ pages/admin/AdminSettingsPage.tsx
@@ -391,7 +391,7 @@ const AdminSettingsPage: React.FC = () => {
                                 </div>
                             )}
                         </div>
-                    </div>
+                    </div> 
                     <div className="bg-white p-6 rounded-lg shadow-md">
                         <h2 className="text-xl font-bold text-gray-800 mb-4 border-b pb-2">Contact Page & Checkout</h2>
                         <div className="space-y-6">
