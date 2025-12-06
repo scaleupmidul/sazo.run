cb59a8065a32d00f486b6b8c7cae42a1d05bc648
scaleupmidul <scaleup.midul@gmail.com>
2025-12-06 20:28:26 +0600

    Update Notification.tsx

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,6 +1,7 @@
 
+
 import React from 'react';
-import { CheckCircle, XCircle } from 'lucide-react';
+import { CheckCircle, XCircle, Info } from 'lucide-react';
 import { Notification as NotificationType } from '../types';
 
 interface NotificationProps {
@@ -10,10 +11,19 @@ interface NotificationProps {
 const Notification: React.FC<NotificationProps> = ({ notification }) => {
   if (!notification) return null;
 
-  const isSuccess = notification.type === 'success';
-  const Icon = isSuccess ? CheckCircle : XCircle;
-  const bgColor = isSuccess ? 'bg-pink-100' : 'bg-red-100';
-  const textColor = isSuccess ? 'text-pink-800' : 'text-red-700';
+  let Icon = CheckCircle;
+  let bgColor = 'bg-pink-100';
+  let textColor = 'text-pink-800';
+
+  if (notification.type === 'error') {
+    Icon = XCircle;
+    bgColor = 'bg-red-100';
+    textColor = 'text-red-700';
+  } else if (notification.type === 'info') {
+    Icon = Info;
+    bgColor = 'bg-blue-100';
+    textColor = 'text-blue-800';
+  }
 
   return (
     <div className={`fixed top-5 right-5 left-5 sm:left-auto max-w-sm mx-auto sm:mx-0 z-50 p-3 sm:p-4 rounded-lg shadow-xl flex items-center space-x-3 transition-opacity duration-300 animate-slideIn ${bgColor} ${textColor}`}>
